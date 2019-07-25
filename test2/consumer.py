d6d6f7a07ee24c7a4dd57dfe7aea7692e46624af
luorui <luorui@meideng.net>
2019-07-25 13:12:58 +0800

    FIX: hints

--- test2/consumer.py
+++ test2/consumer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
 
 consumer = KafkaConsumer('test2', group_id='consumer')
-# 增加了一个 group，在同一个 group 下才生效
+# 增加了一个 group，指定 group id 后 partition 分配才生效
 for record in consumer:
     print(record)
     print('---')
--- test3/consumer.py
+++ test3/consumer.py
@@ -1,11 +1,9 @@
 from kafka import KafkaConsumer
 
-# consumer = KafkaConsumer('test3', group_id='consumer', enable_auto_commit=False)
+consumer = KafkaConsumer('test3', group_id='consumer', enable_auto_commit=False)
 # 每一条消息消费后不自动向 kafka 发送 ack
-consumer = KafkaConsumer('test3', group_id='consumer', enable_auto_commit=True)
+# consumer = KafkaConsumer('test3', group_id='consumer', enable_auto_commit=True)
 
-# 增加了一个 group，在同一个 group 下才生效
-# print('consumer will start at: %s' % consumer.position(TopicPartition('test3', '0')))
 for record in consumer:
     print(record)
     print('---')
